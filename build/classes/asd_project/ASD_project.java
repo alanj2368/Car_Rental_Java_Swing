33b2bda2cfde6d941479f62ea70f73edf153d48a
alanj2368 <alanj2368@gmail.com>
2017-12-14 23:07:08 +0530

    first commit

---
+++ build/classes/asd_project/ASD_project.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asd_project;
+
+/**
+ *
+ * @author ALAN JACOB
+ */
+public class ASD_project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        startPage sp=new startPage();
+        mainPage mp=new mainPage();
+        signin s=new signin();
+        signup sup=new signup();
+        startPage.main(args);
+        
+    }
+    
+}
